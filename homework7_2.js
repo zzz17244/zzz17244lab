db00c5247b6337d653f6d44adde9a642a37c1219
chutintorn srisittikum <zzz17244@gmail.com>
2024-03-09 08:55:40 +0700

    homework7_2

--- homework7_2.js
+++ homework7_2.js
@@ -7,11 +7,16 @@ const dataArray = JSON.parse(jsonData);
 
 const allCovidData = dataArray.reduce( (total,x) =>(total + x.todayRecovered) , 0)
 
-
-console.log (` World today recovers = ${allCovidData} people !`)
+console.log(`***Announcment***`)
+console.log()
+console.log (`1: Now ,Today World  recovers = ${allCovidData} people !`)
 // const newArray2 = newArray.map
+const lowCovidCountry = dataArray.filter(x => x.todayDeaths < 10 &&  x.population > 100000000).map(x=> x.country)
+console.log()
+console.log(`2: Here are the conuntry Death belowed 10 & Population more that 100 millions follow; 
+${lowCovidCountry.join('  ')} , 
 
-const lowCovidCountry = dataArray.filter(x => x.todayDeath < 10 & x.population > 10000000)
-
-console.log(lowCovidCountry)
+Note Total is ${lowCovidCountry.length} Countries  `)
+console.log()
+console.log(`***Reported by Homework7_2*** `)
 
